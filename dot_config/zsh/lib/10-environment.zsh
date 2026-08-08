d1392b6f59bbeb075f6ab1560cc5d6e08ebdbadb
zzy <adris_dreams@outlook.com>
2026-08-08 16:54:34 +0800

    添加系统管理命令目录PATH

--- dot_config/zsh/lib/10-environment.zsh
+++ dot_config/zsh/lib/10-environment.zsh
@@ -9,4 +9,11 @@ for _dotfiles_bin_dir in "$HOME/.local/bin" "$HOME/bin"; do
 done
 path=("${_dotfiles_user_bin_dirs[@]}" $path)
 
-unset _dotfiles_bin_dir _dotfiles_user_bin_dirs
+# 系统管理命令目录PATH
+typeset -a _dotfiles_system_admin_dirs
+for _dotfiles_bin_dir in /usr/local/sbin /usr/sbin /sbin; do
+  [[ -d "$_dotfiles_bin_dir" ]] && _dotfiles_system_admin_dirs+=("$_dotfiles_bin_dir")
+done
+path+=("${_dotfiles_system_admin_dirs[@]}")
+
+unset _dotfiles_bin_dir _dotfiles_user_bin_dirs _dotfiles_system_admin_dirs
